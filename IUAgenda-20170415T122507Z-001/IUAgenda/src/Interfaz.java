1a3960d3ae0bc5beff84431fe9abd03f04dad060
Mario <Mario@192.168.1.39>
2017-04-15 23:21:59 +0200

    Commit de Mario

--- IUAgenda-20170415T122507Z-001/IUAgenda/src/Interfaz.java
+++ IUAgenda-20170415T122507Z-001/IUAgenda/src/Interfaz.java
@@ -121,7 +121,7 @@ public class Interfaz extends javax.swing.JFrame {
         panelPrincipal.setPreferredSize(new java.awt.Dimension(270, 450));
         panelPrincipal.setLayout(new java.awt.CardLayout());
 
-        pantallaPrincipal.setBackground(new java.awt.Color(51, 153, 255));
+        pantallaPrincipal.setBackground(new java.awt.Color(0, 153, 255));
         pantallaPrincipal.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         añadir.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/1492014114_add.png"))); // NOI18N
